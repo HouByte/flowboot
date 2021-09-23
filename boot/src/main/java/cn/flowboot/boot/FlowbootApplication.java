d0a845ae035beecc3932484ef172510032374570
Vincent Vic <vcoding@dingtalk.com>
2021-09-23 20:12:43 +0800

    基本配置

---
+++ boot/src/main/java/cn/flowboot/boot/FlowbootApplication.java
@@ -0,0 +1,19 @@
+package cn.flowboot.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <h1></h1>
+ *
+ * @version 1.0
+ * @author: Vincent Vic
+ * @since: 2021/09/23
+ */
+@SpringBootApplication
+public class FlowbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowbootApplication.class,args);
+    }
+}
